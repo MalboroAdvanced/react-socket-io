e13835f645894db4aadb55f62e8ffd7ac3a27dd3
MalboroAdvanced <brijesh.1995rana@gmail.com>
2021-09-10 15:10:42 +0530

    front-end context and messages done

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
+import { useConversations } from "../contexts/ConversationsProvider";
 
 export default function Conversations() {
-  return <div>CONVERSATIONS</div>;
+  const { conversations, selectConversationIndex } = useConversations();
+  return (
+    <ListGroup variant="flush">
+      {conversations.map((conversation, index) => (
+        <ListGroup.Item
+          action
+          active={conversation.selected}
+          onClick={() => selectConversationIndex(index)}
+          key={index}
+        >
+          {conversation.recipients.map((r) => r.name).join(", ")}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
 }
--- src/components/NewContactModal.js
+++ src/components/NewContactModal.js
@@ -1,13 +1,17 @@
 import React, { useRef } from "react";
 import { Modal, Form, Button } from "react-bootstrap";
+import { useContacts } from "../contexts/ContactsProvider";
 
 export default function NewContactModal({ closeModal }) {
   const idRef = useRef();
   const nameRef = useRef();
+  const { createContact } = useContacts();
 
   function handleSubmit(e) {
+    console.log("Hello");
     e.preventDefault();
-    // createContact(idRef.current.value, nameRef.current.value)
+    createContact(idRef.current.value, nameRef.current.value);
+    closeModal();
   }
 
   return (
@@ -16,7 +20,7 @@ export default function NewContactModal({ closeModal }) {
       <Modal.Body>
         <Form onSubmit={handleSubmit}>
           <Form.Group>
-            <Form.Label>ID:</Form.Label>
+            <Form.Label>ID:=</Form.Label>
             <Form.Control type="text" ref={idRef} required />
           </Form.Group>
           <Form.Group>
